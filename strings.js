32029a9e057429b2eaf757044c178eede1f9b08e
Thomas J. Shavor <thomasshavor@gmail.com>
2016-06-17 10:02:05 -0500

    fixed palindrome function

--- strings.js
+++ strings.js
@@ -1,67 +1,87 @@
-
-
-// Instructions
-
-
-// 3. The text input should only accept letters. No numbers. (WORKS!)
 var inputBox = document.getElementById('inputValue');
 var thisButton = document.getElementById('myButton');
 
+thisButton.addEventListener('click', getValues)
 
-thisButton.addEventListener('click', function (e) {
-	if (inputBox.value.match(/^[a-zA-z]+$/)) {
+// I need to add a part here that accounts for use of the ENTER key!
+
+
+// This functon targets the Id numbers for the input value string on the DOM
+// 3. The text input should only accept letters. No numbers. (WORKS!)
+function getValues() {
+	var enteredString = (document.getElementById("inputValue").value );
+	if (enteredString.match(/^[a-zA-z]+$/)) {
 		alert("Only Letters");
+		console.log("enteredString", enteredString);
+		reversal(enteredString)
+		alphabetize(enteredString)
+		palindrome(enteredString)
+
 	} else{
 		alert("There are Numbers or special characters");
 	}
-})
+}
 
-// This functon targets the Id numbers for the input value string on the DOM
-	// function getValues (){
-	// 	var enteredString = (document.getElementById("inputValue").value );
-	// 	console.log("enteredString", enteredString);
-
-
-// var testString = "";
-// var reversal(testString);
-// var alphabits(testString);
-// var palindrome(testString);
-
+	// Now we run this string through the reversal function:
 // // 4. Implement the logic in the reversal function that reverses the order of the characters in the string, 
 // // and outputs the result in the DOM, below the text input.
 
+// at this point, the function reversal hold the value of the input string and creates a new variable "teststring"
+// and copies the contents or value of enteredString into testString.
+// testStringonly exsits in this function!
+function reversal(testString) {
+	var reversalArray= testString.toLowerCase().split('').reverse();
+	var reversalString = reversalArray.join('');
+	// console.log ("reversal", reversalString);
+	outputToDom(reversalString)
+	return reversalString
+}
 
-// 	function reversal() {
-// 		reversal= testString.reverse();
-// 		console.log ("reversal", reversal);
-// }
+// Now we need to send these results to the DOM...
 
+	// (1)this is the old method of displaying things in the DOM (very unsafe due to the possibility of scripting atacks)
+	// document.getElementById("display-output").innerHTML=whatToDisplay
 
-	// var testString = ["1","2","3","4","5" ];
-	// var reversedTestString= testString.reverse();
-	// console.log("reversedTestString", reversedTestString);
+	// (2)this is a better way of displaying things in the DOM
+	// document.getElementById("display-output").insertAdjacentHTML("afterbegin", whatToDisplay)
 
-	
+	// (3) this is the BEST and SAFEST way to display things in the DOM!!!
+	// created a variable called outputNode that creates an H1 element 
+function outputToDom(whatToDisplay) {
+	var outputNode = document.createElement("h1");
+	// created another variable called "outputText" that will be what I place in the h1 element.
+	var outputText = document.createTextNode(whatToDisplay);
 
-
+	// these last 2 lines then create and place the value of
+	outputNode.appendChild(outputText);
+	document.getElementById("calcValues").appendChild(outputNode);
+}
 // // 5. Implement the logic in the alphabits function that return the characters in alphabetical order, 
 // // and outputs the result in the DOM, below the text input.
 // function alphabits() {
-
-// }
+function alphabetize(testString2) {
+	var alphabetizeString= testString2.toLowerCase().split('').sort().join('');
+	// console.log ("alphabetize", alphabetizeString);
+	outputToDom(alphabetizeString)
+} 
 // // 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. 
 // // If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
-// function palindrome() {
 
-// var isPalindrome = function (string) {
-//     if (string == string.split('').reverse().join('')) {
-//         alert(string + ' is palindrome.');
-//     }
-//     else {
-//         alert(string + ' is not palindrome.');
-//     }
-// }
-// }
+function palindrome (testString3){ 
+	var isPalindrome = false;
+	var reverseTestString = reversal(testString3);
+
+    if (testString3 === reverseTestString) {
+        alert(testString3 + ' is palindrome.');
+        isPalindrome = true;
+    }
+    else {
+        alert(testString3 + ' is not palindrome.');
+    }
+
+    outputToDom(isPalindrome)
+    return isPalindrome;
+}
 
 // // 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString 
 // // variable (see below) to the value of the input.
